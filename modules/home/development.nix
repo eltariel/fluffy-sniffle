1979caebfcb9ec7a9991a309d29b17e08de35309
Ellie T <ellie@eltariel.com>
2025-02-18 13:27:44 +1100

    vscode why not

--- modules/home/development.nix
+++ modules/home/development.nix
@@ -3,6 +3,10 @@
     jq.enable = true;
     pyenv.enable = true;
 
+    vscode = {
+      enable = true;
+    };
+
     pyenv = {
       rootDirectory = "${config.home.homeDirectory}/.pyenv";
     };
