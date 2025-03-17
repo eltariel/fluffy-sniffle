bc59b005129dea452d9f08efca2c48da1fb52b64
Ellie T <ellie@eltariel.com>
2025-03-17 18:28:49 +1100

    Refactor UI modules

--- modules/nixos/ui/gnome.nix
+++ modules/nixos/ui/gnome.nix
@@ -1,23 +1,8 @@
 {pkgs, ...}: {
-  services.xserver.enable = true;
+  imports = [ ./. ];
 
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  services.flatpak = {
-    enable = true;
+  services.xserver = {
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
   };
-  xdg.portal = {
-    enable = true;
-    # config = { ... };
-  };
-
-  programs.appimage = {
-    enable = true;
-    binfmt = true;
-  };
-
-  environment.systemPackages = with pkgs; [
-    firefox
-  ];
 }
