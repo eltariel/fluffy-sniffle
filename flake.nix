51c5f22234112fc0fab36770a60abb7121edc129
Ellie T <ellie@eltariel.com>
2025-11-24 18:04:24 +1100

    nix-darwin for the intel macs

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -11,18 +15,19 @@
   };
 
   outputs = {
-    self,
     nixpkgs,
     nixos-hardware,
     home-manager,
+    nix-darwin,
     ...
   } @ inputs: let
     inherit (nixpkgs.lib) genAttrs;
 
     x86_64-linux = "x86_64-linux";
     aarch64-linux = "aarch64-linux";
+    x86_64-darwin = "x86_64-darwin";
     aarch64-darwin = "aarch64-darwin";
-    allSystems = [x86_64-linux aarch64-linux];
+    allSystems = [x86_64-linux aarch64-linux x86_64-darwin aarch64-darwin];
 
     systemPkgs = genAttrs allSystems (
       system:
@@ -43,6 +48,7 @@
       };
       darwin = {
         e1m1 = aarch64-darwin;
+        e1i2 = x86_64-darwin;
       };
       homeManager = {
         pb2-2 = aarch64-linux;
@@ -61,6 +67,30 @@
     formatter = genAttrs allSystems (s: nixpkgs.legacyPackages.${s}.alejandra);
 
     nixosConfigurations = hostBuilder.nixosFor hosts.nixos defaultUsers;
+    darwinConfigurations = {
+      e1i2 = nix-darwin.lib.darwinSystem {
+        system = x86_64-darwin;
+        pkgs = systemPkgs."x86_64-darwin";
+        modules = [
+          ./hosts/darwin/e1i2
+		  ./modules/darwin/ui
+
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.ellie = {
+                imports = [
+                  ./users/common/ui
+                  ./users/ellie.nix
+                ];
+              };
+            };
+          }
+        ];
+      };
+    };
 
     homeConfigurations =
       nixLinux aarch64-linux "pb2-2" ["ellie"]
---
+++ hosts/darwin/e1i2/default.nix
@@ -0,0 +1,19 @@
+    { pkgs, ... }: {
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      #system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+#      nixpkgs.hostPlatform = "x86_64-darwin";
+
+          users.users.ellie = { name = "ellie"; home = "/Users/ellie"; };
+    }
