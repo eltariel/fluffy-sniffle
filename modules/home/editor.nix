660df61acdcfecf5f69a3081e6a8570a04846234
Ellie T <ellie@eltariel.com>
2026-07-30 19:15:20 +1000

    fix neovim config

--- modules/home/editor.nix
+++ modules/home/editor.nix
@@ -6,11 +6,16 @@
   };
 
   programs = {
-    neovim.enable = true;
-
     neovim = {
+      enable = true;
+
       vimAlias = true;
       vimdiffAlias = true;
+
+      sideloadInitLua = true; # don't manage/overwrite init.lua
+
+      withPython3 = true;
+      withRuby = false;
     };
   };
 }
